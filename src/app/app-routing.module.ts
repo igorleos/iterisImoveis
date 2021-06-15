a92906b335ef77635fb8c56591c620f4a86305c2
Spurng <igor-k2010@hotmail.com>
2021-06-15 13:13:31 -0400

    iterisImoveis base project running

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ImoveisPageComponent } from './components/imoveis-page/imoveis-page.component';
+import { MenuTemplateComponent } from './components/menu-template/menu-template.component';
+import { SobrePageComponent } from './components/sobre-page/sobre-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:MenuTemplateComponent,
+    children:[
+      {
+        path:'',
+        component:HomePageComponent,
+      },
+
+      {
+        path:'imoveis',
+        component:ImoveisPageComponent
+      },
+
+      {
+        path:'sobre',
+        component:SobrePageComponent,
+      }
+    ]
+
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

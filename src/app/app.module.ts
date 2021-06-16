88a008bde5024f4c1b8b05c0f945179aa384cf1b
Spurng <igor-k2010@hotmail.com>
2021-06-16 11:16:35 -0400

    slideshow working, testing transitions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,7 +41,8 @@ import { ImovelSlideComponent } from './components/imovel-slide/imovel-slide.com
     MatIconModule,
     MatListModule,
     HttpClientModule,
-    MatCardModule
+    MatCardModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
